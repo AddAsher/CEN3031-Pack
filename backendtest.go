31d331ac4d5bd0ca6fb107e434c90a73e8837643
JC Mone <john.mone@ufl.edu>
2023-02-08 01:24:35 -0500

    added struct + more comments

--- backendtest.go
+++ backendtest.go
@@ -3,7 +3,34 @@ package main
 import "fmt"
 
 //to test use test@ufl.edu and test
+
+type Club struct { //We'll use this when we have a CSV, for now we can just use println to show how it would function.
+	 name string 
+	 description string
+}
+ //Club clubs := []Club{}
+
 func main() {
+//club1 := Club{
+//	name:  "360HM", description: "The purpose of the 360BHM is to this series seeks to educate, enhance, and entertain the university and Gainesville communities by reflecting on the contributions that Black Americans have made. It is our goal to advocate exposure of refined enrichment inspired by the heritage and legacy of universal Black culture."}
+
+
+
+
+
+//clubVector = append(clubs, club1)
+
+//for i := 0; i < len(clubVector{}); i++ {
+//	fmt.Println(clubVector[i].name);
+//}
+
+//we put the csv stuff here but we need that data to import
+
+
+
+
+
+
 	fmt.Println("Welcome to Pack!")
 	fmt.Println("Please Enter your email and password") //just make sure email has ufl, later tho
 	fmt.Println("Email:")
@@ -33,7 +60,7 @@ func main() {
 	}
 	var menuDone bool = false
 	var subMenu bool = false
-
+	
 	for !menuDone {
 		fmt.Println("Please choice an option.")
 		fmt.Println("1. View Club Hostings")
