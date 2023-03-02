129e40252c801271287f2c62598e522a3363ae17
AddAsher <asheram2013@gmail.com>
2023-03-01 23:45:39 -0500

    Working connection
    Connected frontend and backend

--- SWE_Proj/src/app/app.component.ts
+++ SWE_Proj/src/app/app.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class AuthService {
+
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    const url = `${this.baseUrl}/login`;
+    const body = { username, password };
+    return this.http.post<User>(url, body);
+  }
+
+}
 
 @Component({
   selector: 'app-root',
+  template: `
+  <ul>
+      <li *ngFor="let user of users">{{ user.name }}</li>
+  </ul>
+`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PACK';
 
-  buttonPress()
-  {
-    alert("Login Button Clicked")
+
+export class AppComponent {
+  email: string = "admin";
+  password: string = "password";
+  username: string = "admin";
+  title = 'PACK';
+  users: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    this.http.post('http://localhost:8080/login', {
+      email: this.email,
+      password: this.password
+    }).subscribe((response) => {
+      console.log("Login request recieved");
+    }, (error) => {
+      console.log(error);
+    });
   }
 }
--- SWE_Proj/src/app/app.module.ts
+++ SWE_Proj/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
