938089705a0a57959021c04aaf1e11689bdc5466
Juan Rodriguez <rodriguezjuan@ufl.edu>
2023-03-29 21:43:59 -0400

    asd

--- SWE_Proj/src/app/Registration/registration.component.ts
+++ SWE_Proj/src/app/Registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({ templateUrl: 'registration.component.html' })
-export class RegistrationComponent {}
+export class RegistrationComponent {
+    title = 'PACK';
+}
