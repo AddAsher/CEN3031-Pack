3333a746a38630f5eb16c9a37a65bd372b028992
jaydenj21 <81257951+jaydenj21@users.noreply.github.com>
2023-04-13 23:56:43 -0400

    Some css changes and registration functionality added

--- frontEnd/src/app/registration/registration.component.ts
+++ frontEnd/src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import {Component} from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  rmsg: string;
+  username: string;
+  password: string;
 
+  constructor(private authService: AuthService, private router: Router){
+    this.rmsg="Register here";
+    this.username="";
+    this.password="";
+  }
+
+
+  onSubmit(){
+    if(this.password.length < 8){
+      window.alert("Password is too short!");
+    }
+    /*Send login info to backend and check to make sure the new username doesn't already exist
+    in the backend map. If it does, say that username already exists and navigate back to login. */
+    /*If it doesn't exist send the login info to be added to the backend and redirect user back to login screen
+    to enter their new credentials*/
+  }
+  goToLogin(){
+    this.router.navigate(['']);
+  }
 }
