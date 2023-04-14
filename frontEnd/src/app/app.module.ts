0b7a44b4177cac8f1e19fb93b75ad01f43771059
jaydenj21 <81257951+jaydenj21@users.noreply.github.com>
2023-04-13 21:09:13 -0400

    Routing works as intended now

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AuthService } from './auth.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     HomePageComponent,
     RegistrationComponent
   ],
--- frontEnd/src/app/home-page/home-page.component.ts
+++ frontEnd/src/app/home-page/home-page.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  //variables to be used to construct a search bar
+  searchTerm: string;
+  searchResults: any[];
+  welcomeMessage: string;
+
+  constructor() {
+    this.searchTerm = "";
+    this.searchResults = [];
+    this.welcomeMessage =  "Find the club that's right for you!";
+  }
+
+  performSearch() {
+    //1. retrieve list of clubs stored in backend to be put into an array
+    //2. search that array for the name if it is present
+    //3. if found go from there
+         //if not create a popup saying 'Club not yet in database'
+    throw new Error('Method not implemented.');
+  }
 }
