ce75de5086b05819f45d628087737e0d16a50719
Juan Rodriguez <rodriguezjuan@ufl.edu>
2023-04-19 21:53:05 -0400

    updated home page/added additional routing btns

--- frontEnd/src/app/login/login.component.ts
+++ frontEnd/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
